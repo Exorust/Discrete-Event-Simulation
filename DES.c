a2a05cbcbdf65dfb57a2c1c927a9cb638bb28ea5
Exorust <chandrahas.aroori@gmail.com>
2018-10-25 20:52:25 +0530

    Base of DES

--- DES.c
+++ DES.c
@@ -1,5 +1,16 @@
-#include"include/event_queue.h"
-#include"include/process_table.h"
+// #include"include/event_queue.h"
+// #include"include/process_table.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+int main(int argc, char const *argv[]) {
+  FILE * fp;
+  fp = fopen("process.csv", "r");
+  int arrt,burst;
+  do {
+    fscanf(fp,"%d,%d", &arrt, &burst);
+  } while(!feof(fp));
+  fclose(fp);
+  return 0;
+}
